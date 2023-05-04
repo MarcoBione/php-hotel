c6bd8d8253746bc21e23d2fa38cba3ae1b855ebf
MarcoBione <marcobione.work@gmail.com>
2023-05-04 17:10:18 +0200

    Add table with informations

--- index.php
+++ index.php
@@ -48,16 +48,61 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
 
-    <div>
+
+    <!-- cicle stamp for all data -->
+    <!-- <div>
         <?php foreach ($hotels as $hotel) {
             var_dump($hotel);
         }
         ?>
+    </div> -->
+
+    <!-- table -->
+    <div class="container py-5">
+
+        <table class="table table-striped">
+            <!-- category titles -->
+            <thead>
+                <tr>
+                    <th scope="col">Hotel</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+
+            <tbody class="table-group-divider">
+                <!-- cicle stamp -->
+                <?php foreach ($hotels as $hotel) { ?>
+
+                    <tr>
+                        <td><?php echo $hotel['name'] ?></td>
+                        <td><?php echo $hotel['description'] ?></td>
+                        <td><?php
+                            if ($hotel['parking']) {
+                                echo 'Sì';
+                            } else {
+                                echo 'No';
+                            } ?>
+                        </td>
+                        <td><?php echo $hotel['vote'] ?></td>
+                        <td><?php echo $hotel['distance_to_center'] ?> km</td>
+                    </tr>
+
+                <?php } ?>
+                <!-- end cicle stamp -->
+            </tbody>
+
+        </table>
     </div>
+    <!-- end table -->
+
 
 </body>
 
